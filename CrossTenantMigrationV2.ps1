71d0b03c8f4da58a789a54442c0e078c305e950a
nhyiraba <nhyiraba@yahoo.com>
2022-02-12 16:56:01 +0800

    CrossTenantMigrationV2

--- CrossTenantMigrationV2.ps1
+++ CrossTenantMigrationV2.ps1
@@ -74,6 +74,7 @@ function Get-CSVFile {
     return $OpenFileDialog.FileName
 }
 
+
 function CrossT2TMigration {
   
     <#
@@ -135,7 +136,7 @@ function CrossT2TMigration {
 #>
 
 #Creating mail user on source tenant
-function CreateOneTragetMailUser {
+function New-TragetMailUser {
    
     [CmdletBinding(DefaultParameterSetName = "Default")]
     param (
@@ -165,7 +166,7 @@ function CreateOneTragetMailUser {
 
 #Creating mail user on source tenant
 
-function CreateBulkTargetMailUser {
+function New-BulkTargetMailUser {
 
     
     [CmdletBinding(DefaultParameterSetName = "Default")]
@@ -193,6 +194,7 @@ function CreateBulkTargetMailUser {
         
 }
 
+
 <#
     This function is designed to retrive user propertions for a given mailbox
     The mailbox or mailboxes can be supplied inline data seperated by comma or 
@@ -201,7 +203,35 @@ function CreateBulkTargetMailUser {
     Restults:
         This will return a table formated file and can be exported as csv file.
 #>
-function ExtractExchGUIandX500 {
+
+#for retrieving the mailbox information
+function Get-MailboxRequiedInfo {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [array]
+        $UserMailbox
+    )
+    
+    # get mailbox information  and return array object
+    $MailBoxInfo = @()
+    $UserMailbox | ForEach-Object {
+        Get-MailBox -Identity $_ | Select-Object DisplayName, PrimarySmtpAddress, ExchangeGuid, LegacyExchangeDN
+
+        $Info = [PSCustomObject]@{
+            DisplayName      = $_.DisplayName
+            ExchangeGuid     = $_.ExchangeGuid
+            EmailAddress     = $_.PrimarySmtpAddress
+            LegacyExchangeDN = $_.LegacyExchangeDN
+        }
+
+        $MailBoxInfo += $Info
+    }
+
+    return $MailBoxInfo
+}
+
+function Get-ExchGUIandX500FromSource {
 
     [CmdletBinding(DefaultParameterSetName = "Default")]
     param (
@@ -215,29 +245,6 @@ function ExtractExchGUIandX500 {
         [bool]
         $ExportResultAs = $false
     )
-
-    #for retrieving the mailbox information
-    function RetrievMailboxInfo {
-        [CmdletBinding()]
-        param (
-            [Parameter(Mandatory)]
-            [array]
-            $UserMailbox
-        )
-        
-        # get mailbox information  and return array object
-        $UserMailbox | ForEach-Object {
-            Get-MailBox -Identity $_ | Select-Object DisplayName, PrimarySmtpAddress, ExchangeGuid, LegacyExchangeDN
-
-            $MailBoxInfo = [PSCustomObject]@{
-                DisplayName      = $_.DisplayName
-                ExchangeGuid     = $_.ExchangeGuid
-                EmailAddress     = $_.PrimarySmtpAddress
-                LegacyExchangeDN = $_.LegacyExchangeDN
-            }
-        }
-    }
-
 
     #requesting user to input mailbox email address or displayname if not specified
     # this provides flexibility to specify the paramter when calling the function independently
@@ -256,7 +263,7 @@ function ExtractExchGUIandX500 {
 
                 if ($BulkOrOneMailbox.Length -ge 1) {
                     #get single mailbox information
-                    $MailboxInfoResults = RetrievMailboxInfo -UserMailbox $BulkOrOneMailbox
+                    $MailboxInfoResults = Get-MailboxRequiedInfo -UserMailbox $BulkOrOneMailbox
                     return $MailboxInfoResults 
                 }
                 else {
@@ -269,7 +276,7 @@ function ExtractExchGUIandX500 {
                 $getCsvData = Get-CSVFile #get file
                 $readCsvData = Get-Content -Path $getCsvData
                     
-                $MailboxInfoResults = RetrievMailboxInfo -UserMailbox $readCsvData
+                $MailboxInfoResults = Get-MailboxRequiedInfo -UserMailbox $readCsvData
                 return $MailboxInfoResults
             
             }
@@ -283,7 +290,7 @@ function ExtractExchGUIandX500 {
 
         if ($BulkOrOne.Length -ge 1) {
             #get single mailbox information
-            $MailboxInfoResults = RetrievMailboxInfo -UserMailbox $BulkOrOne
+            $MailboxInfoResults = Get-MailboxRequiedInfo -UserMailbox $BulkOrOne
             return $MailboxInfoResults 
         }
         else {
@@ -293,23 +300,87 @@ function ExtractExchGUIandX500 {
 
 }
 
-function ExchGuidx500TranferSourceToTarget {
+
+function ValidateProperties {
+    #Valid the presence of "DisplayName","LegacyExchangeDN","ExchangeGuid"
+    param (
+        [Parameter(Mandatory)]
+        [array]
+        $MailboxDataSet
+    )
+
+    $columns = ($MailboxDataSet | Get-Member).Name
+    $requireColumn = "DisplayName", "LegacyExchangeDN", "ExchangeGuid"
+
+    return (($requireColumn[0] -in $columns) -and ($requireColumn[1] -in $columns) -and ($requireColumn[2] -in $columns)) 
+}
+
+function Update-ExchGuidx500ToTarget {
+
+    <#
+    .Synopsis
+    Transfer mailbox information from source tanant to target tenant. Must be connected to TARGET TENANT exchange online
+    .DESCRIPTION
+    This
+    .EXAMPLE
+    Tranfer-ExchGuidx500ToTarget -UserMailbox <Array of Mailboxes>
+    .FUNCTIONALITY
+    Select each mailbox in the arrays of mailboxes and stamps or transfer LegacyExchangeDN and ExchangeGuid from
+    the source mailbox to target tenant's corresponding mailuser mapping by ExchangeGuid 
+    #>
+
+    [CmdletBinding()]
+    param (
+        # This is an array of mailbox information retrieved from the source Tangent.
+        # The array column must include the following
+        # if this parameter is NOT defined, you will be prompt to select the csv file that contains mailbox info from SOURCE TENANT
+        # which contain all the required columns
+        [Parameter(Mandatory = $false)]
+        [array]
+        $ExtractedUMailBoxInfo
+
+    )
+    process {
+
+        If (-not($ExtractedUMailBoxInfo)) {
+            # get and import mailbox info
+            $ExtractedUMailBoxInfo = Import-csv -Path (Get-CSVFile)
+        }
+       
+        # data column validation and transfer
+        if ((ValidateProperties -MailboxDataSet $ExtractedUMailBoxInfo) -eq $true) {
+
+            Write-Host "`tLegacyExchangDN  and ExchangeGUID proprty transfer started...........`n" -ForegroundColor Green
+            #loop through all the mailbox
+            $ExtractedUMailBoxInfo | ForEach-Object {
+                Set-MailUser -identity $_.DisplayName -ExchangeGuid $_.ExchangeGUID -EmailAddresses @{add="X500:" + $_.LegacyExchangDN } 
+            }
+            Write-Host "`t`n....LegacyExchangDN  and ExchangeGUID proprty transfer started.`n" -ForegroundColor Green
+
+        }
+        else {
+            Write-Host "Invalid the data conlumn format" -ForegroundColor Red
+        }
+    }
+    
+}
+
+function TranferSourceToTarget {
 
     [CmdletBinding(DefaultParameterSetName = "Default")]
     param (
         # SourceTenant, This paramter is mandatory and used in command prefix for destinguish destination and source
-        [Parameter(Mandatory, ParameterSetName = "Name") 
-        ]
+        [Parameter(Mandatory)]
         [string]
-        $Name,
+        $SourceTenantName,
 
         # Target or Destination Tenant. This paramter is mandatory and used in command prefix for destinguish destination and source
-        [Parameter(Mandatory, ParameterSetName = "TargetTenantName")]
+        [Parameter(Mandatory)]
         [String]
         $TargetTenantName,
 
         # sepecify the migration security group from source tenant, this is also called the migration scope
-        [Parameter(Mandatory, ParameterSetName = "SourceTenantMigratSecurityGroup")]
+        [Parameter(Mandatory)]
         [String]
         $SourceTenantMigratSecurityGroup,
 
@@ -319,12 +390,14 @@ function ExchGuidx500TranferSourceToTarget {
         $TargetTenantMigratSecurityGroup,
 
         #this is not mandatory and can be specified for the all mailusers for migration not yet create on target tenant
-        [Parameter(Mandatory, ParameterSetName = "UserAlreadyExist")]
-        [bool]
-        $UserAlreadyExist = $true,
+        [Parameter(Mandatory, ParameterSetName = "UsersDoNotExistOnTarget")]
+        [switch]
+        $UsersDoNotExistOnTarget,
 
         #specify domain for create new mail user on target tenant
-        [Parameter(Mandatory = $false, ParameterSetName = "UserAlreadyExist")]
+        [Parameter(Mandatory, ParameterSetName = "UsersDoNotExistOnTarget",
+            HelpMessage = "Domain on which mail user should be created")]
+        [Parameter(Mandatory = $false)]
         [string]
         $TargetDomainForNewMailUsers
     )
@@ -336,7 +409,7 @@ function ExchGuidx500TranferSourceToTarget {
         Automated process
         ===========================
             Get source properties using 
-                $userpros = ExtractExchGUIandX500 function
+                $userpros = ExtractExchGUIandX500 functiFromSourceon
             Connect to target tenant
                 Connect-exchangeOnline #target tenant admin
             Use the properties from $userpros to stamp it on the target mail users
@@ -348,7 +421,7 @@ function ExchGuidx500TranferSourceToTarget {
         Another way is to manually map the object from the destination to source properties
         
         To get source properties use
-            ExtractExchGUIandX500, 
+            Extract-ExchGUIandX500FromSource, 
             export the results as csv
             Open the csv file
             Create new column and the mail user emails address from the target tenant.
@@ -386,70 +459,48 @@ function ExchGuidx500TranferSourceToTarget {
 
     #get all the equivalent mail users from destination or target tenant.
     
-    # Propertity check
-    If ($UserAlreadyExist -eq $false) {
+    # Propertity check.
+    # If the $UsersDoNotExistOnTarget is set to false, the objects will be created automatically on the target tenant.
+    If ($UsersDoNotExistOnTarget) {
+        
+        # showing instruction
         Write-Host $ObjectCreationOnTarget
-        $ChoseDomain = Read-Host "ENTER DOMAIN NAME FOR OBJECT CREATION  "
+        $TargetDomainForNewMailUsers 
 
-        if ($ChoseDomain -notin (Invoke-Expression("get-" + $TargetTenantName + "AcceptedDomain")).DomainName ) {
+        # validating domain if included in accepted domains. if not in included, select the default domain
+        if ($TargetDomainForNewMailUsers -notin (Invoke-Expression("get-" + $TargetTenantName + "AcceptedDomain")).DomainName ) {
             $allDomains = (Invoke-Expression("get-" + $TargetTenantName + "AcceptedDomain")) | Select-Object DomainName, Default
             Write-Host "`nThe domain provided in not included in your accepted domains. `nYour accepted domains are : `n`nDOMAINS`n===================="
-            Write-Output $allDomains
-            
-        }
-        else {
+            $allDomains | Out-String | ForEach-Object { Write-Host $_ }
+
             #selected domain
-            $defaultDomain = ($allDomains | where-object { $_.Default -eq "True" }).DomainName
-            Write-host "`n`nSetting mail user creation domain to the tenant default domain : " + $defaultDomain
-
-
-            Write-Host " ========================== Creating Mail Users on Target Tanant ========================"
-
-            $MailUserPwd = Read-Host "ENTER DEFAULT PASSWORD FOR NEW MAIL USER CREATION ON TARGET " 
-            
-            Write-host "`n`n"
-
-            $MailboxInfoAll | ForEach-Object{
-                $MailUserAddress = ($MailUserDetail.PrimarySmtpAddress.split("@")[0]+"@"+$defaultDomain).ToLower() #split and remove all white spaces from the imput
-
-                #create  mail user with entered data
-                Invoke-Expression("New-"+$TargetTenantName+"MailUser -Name "+$_.DisplayName+" -MicrosoftOnlineServicesID "+$_.MailUserAddress+" -DisplayName "+$_.DisplayName+" -Password (ConvertTo-SecureString -String "+$MailUserPwd+" -AsPlainText -Force)")
-            }
+            $TargetDomainForNewMailUsers = ($allDomains | where-object { $_.Default -eq "True" }).DomainName
+            Write-host "`n`nSetting mail user creation domain to the tenant default domain : " + $TargetDomainForNewMailUsers
         }
+        
+        Write-Host "`n ========================== Creating Mail Users on Target Tanant ========================`n"
+        $MailUserPwd = Read-Host "ENTER DEFAULT PASSWORD FOR NEW MAIL USER CREATION ON TARGET " 
+        
+        Write-host "`n`n"
+
+        $MailboxInfoAll | ForEach-Object {
+            $MailUserAddress = ($MailUserDetail.PrimarySmtpAddress.split("@")[0] + "@" + $TargetDomainForNewMailUsers).ToLower() #split and remove all white spaces from the imput
+
+            #create  mail user with entered data
+            Invoke-Expression("New-" + $TargetTenantName + "MailUser -Name " + $_.DisplayName + " -MicrosoftOnlineServicesID " + $MailUserAddress + " -DisplayName " + $_.DisplayName + " -Password (ConvertTo-SecureString -String " + $MailUserPwd + " -AsPlainText -Force)")
+        }
+    
     }
 
     # Get all the scope mail user from the target and stamp them with Exchange GUID and Lagacy DN from source.
-    
-
-
-
+    Write-Host "`tLegacyExchangDN  and ExchangeGUID proprty transfer started...........`n" -ForegroundColor Green
+    #loop through all the mailbox
+    $MailboxInfoAll | ForEach-Object {
+        Invoke-Expression( "Set-" + $TargetTenantName + "MailUser -identity " + $_.DisplayName + " -ExchangeGuid " + $_.ExchangeGUID + " -EmailAddresses @{add=" + "`"X500:" + $_.LegacyExchangeDN + "`"}")
+    }
+    Write-Host "`t`n....LegacyExchangDN  and ExchangeGUID proprty transfer started.`n" -ForegroundColor Green
 
 }
 
 
-
     
-
-
-
-#pass the list of user the ExtractExchGUIandX500 fountion
-
-
-
-    
-    
-    
-
-
-    
-    
-
-
-
-
-
-
-
-
-    
-}
